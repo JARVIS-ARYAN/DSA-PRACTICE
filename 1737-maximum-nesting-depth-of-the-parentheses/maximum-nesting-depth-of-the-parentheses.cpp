0b88f9ef68c5ee40ebf69b0b66d74bd8bd2bb1ec
JARVIS-ARYAN <shivbajpai338@gmail.com>
2026-09-16 11:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (60.45%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char> st;
+        int current_depth = 0;
         int maxdepth = 0;
 
         for (char ch : s) {
             // 1. If it's an opening bracket, push to stack
             if (ch == '(' ) {
-                st.push(ch);
-                if(st.size() > maxdepth){
-                    maxdepth = st.size();
+                current_depth++;
+                if(current_depth > maxdepth){
+                    maxdepth = current_depth;
                 }
             } 
             // 2. If it's a closing bracket
             else if((ch == ')')) {
-                st.pop();
+                current_depth--;
                 
             
             }
